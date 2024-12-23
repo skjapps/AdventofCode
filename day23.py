b051967170c9a4833700cd06f78f25afca38c9c3
skjapps <srikanthjakka@hotmail.co.uk>
2024-12-23 14:46:36 +0000

    updatedday23

--- day23.py
+++ day23.py
@@ -1,60 +1,77 @@
+from itertools import combinations
+import threading
 from useful_class import UsefulClass
-import re
-import time
 
-def day23pt1():
-    connections = UsefulClass.read_lines_to_array("assets/txt/day23")
-    connections_with_chief_computer = []
-    for connection in connections:
-        computer1, computer2 = map(str, re.split(r"-", connection))
-        if re.match(r"^t", computer1) or re.match(r"^t", computer2):
-            connections_with_chief_computer.append(connection)
-    # print(connections_with_chief_computer)
-    one_ways = []
-    counter = 0
-    for connection in connections_with_chief_computer:
-        # one_ways.append([])
-        computer1, computer2 = map(str, re.split(r"-", connection))
-        for test_connections in connections_with_chief_computer:
-            computer1test, computer2test = map(str, re.split(r"-", test_connections))
-            # Same Connection
-            if computer1 == computer1test and computer2 == computer2test:
-                pass
-            # A link between one computer
-            elif (computer1 == computer1test and computer2 != computer2test):
-                one_way = computer2+","+computer1+","+computer1test
-                # one_ways[counter].append(one_way)
-                one_ways.append(one_way)
-            elif (computer1 != computer1test and computer2 == computer2test):
-                one_way = computer1+","+computer2+","+computer1test
-                # one_ways[counter].append(one_way)
-                one_ways.append(one_way)
-            else:
-                pass
-        # counter += 1
-    # print(one_ways)
-    # Pruning useless ones...?
-    one_ways_pruned = []
-    for one_way in one_ways:
-        computer1, computer2, computer3 = map(str, re.split(",", one_way))
-        if computer1 == computer2 or computer2 == computer3 or computer3 == computer1:
-            pass
-        else:
-            one_ways_pruned.append(one_way)
-    # print(one_ways_pruned)
-    # Find full loops...
-    full_loops = []
-    for one_way in one_ways_pruned:
-        computer1, computer2, computer3 = map(str, re.split(",", one_way))
-        for connection in connections:
-            connection1, connection2 = map(str, re.split("-", connection))
-            # If the edge computers are linked
-            if (computer1 == connection1 and computer3 == connection2) \
-                or (computer1 == connection2 and computer3 == connection1):
-                    full_loops.append(one_way)
-    print(full_loops)
-    print(len(full_loops))
+def day23pt1(network_map):
+    # Parse the network map into a dictionary of connections
+    connections = {}
+    for connection in network_map:
+        a, b = connection.split('-')
+        if a not in connections:
+            connections[a] = set()
+        if b not in connections:
+            connections[b] = set()
+        connections[a].add(b)
+        connections[b].add(a)
 
+    # Find all sets of three inter-connected computers
+    sets_of_three = []
+    for a, b, c in combinations(connections.keys(), 3):
+        if b in connections[a] and c in connections[a] and c in connections[b]:
+            sets_of_three.append({a, b, c})
 
+    # Filter sets to include only those with at least one computer starting with 't'
+    filtered_sets = [s for s in sets_of_three if any(computer.startswith('t') for computer in s)]
 
-day23pt1()
+    # Print the results
+    print(f"Total sets of three inter-connected computers: {len(sets_of_three)}")
+    print(f"Sets containing at least one computer starting with 't': {len(filtered_sets)}")
+
+def day23pt2(network_map):
+    # Parse the network map into a dictionary of connections
+    connections = {}
+    for connection in network_map:
+        a, b = connection.split('-')
+        if a not in connections:
+            connections[a] = set()
+        if b not in connections:
+            connections[b] = set()
+        connections[a].add(b)
+        connections[b].add(a)
+
+    # Function to check if a set of computers is fully connected
+    def is_clique(clique, connections, result):
+        if all(connections[a].issuperset(clique) for a in clique):
+            result.append(set(clique))
+
+    # Function to find the largest set of fully connected computers (clique)
+    def find_largest_clique(connections):
+        max_clique = set()
+        result = []
+        threads = []
+        for size in range(len(connections), 2, -1):
+            for clique in combinations(connections.keys(), size):
+                thread = threading.Thread(target=is_clique, args=(clique, connections, result))
+                threads.append(thread)
+                thread.start()
+            for thread in threads:
+                thread.join()
+            if result:
+                return result[0]
+        return max_clique
+
+    # Find the largest clique
+    largest_clique = find_largest_clique(connections)
+
+    # Generate the password by sorting the names alphabetically and joining them with commas
+    password = ','.join(sorted(largest_clique))
+
+    # Print the password
+    print(f"The password to get into the LAN party is: {password}")
+
+# Define the network map as a list of connections
+network_map = UsefulClass.read_lines_to_array("assets/txt/day23")
+
+day23pt1(network_map)
+
+day23pt2(network_map)
